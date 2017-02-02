aa4032a88fcd35e8cdbead3d7e58e21d1fd69d24
emmanuelmugogo <mugoclan87@gmail.com>
2017-02-02 15:42:39 -0500

    Added code to the main class to pass the test

--- src/main/java/org/wecancodeit/exercise/ohmy/OhMyIterator.java
+++ src/main/java/org/wecancodeit/exercise/ohmy/OhMyIterator.java
@@ -1,4 +1,33 @@
 package org.wecancodeit.exercise.ohmy;
 
-public class OhMyIterator {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class OhMyIterator implements Iterator<String> {
+
+	private String[] strings;
+	private int index;
+
+	public OhMyIterator(String[] strings) {
+		this.strings = strings;
+		
+	}
+
+	@Override
+	public boolean hasNext() {
+		return index < strings.length;
+	}
+
+	@Override
+	public String next() {
+		
+		if( !hasNext()) {
+			throw new NoSuchElementException();
+		
+		}
+		String currentString = strings[index];
+		index++;
+		return currentString;
+		
+	}
 }
